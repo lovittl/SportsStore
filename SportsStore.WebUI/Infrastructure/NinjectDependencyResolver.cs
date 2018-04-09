33958e4e3ee127c01ec222d997eb5d81f7815119
Roy Lovitt <lovittl@my.erau.edu>
2018-04-09 11:40:31 -0400

    Add Ninject Bindings for IOrderProcessor tot eh NinjectDependencyResolver.cs file in the WebUI/Infrustructure folder. Close #46

--- SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -8,6 +8,8 @@ using SportsStore.Domain.Abstract;
 using SportsStore.Domain.Entities;
 using System.Web;
 using SportsStore.Domain.Concrete;
+using System.Configuration;
+
 
 namespace SportsStore.WebUI.Infrastructure
 {
@@ -32,13 +34,16 @@ namespace SportsStore.WebUI.Infrastructure
 
         private void AddBindings()
         {
-            Mock<IProductRepository> mock = new Mock<IProductRepository>();
-            mock.Setup(m => m.Products).Returns(new List<Product> {
-                              new Product { Name = "Football", Price = 25 },
-                new Product { Name = "Surf board", Price = 179 },
-                 new Product { Name = "Running shoes", Price = 95 }
-                          });
-            kernel.Bind<IProductRepository>().ToConstant(mock.Object);
+            kernel.Bind<IProductRepository>().To<EFProductRepository>();
+
+            EmailSettings emailSettings = new EmailSettings
+            {
+                WriteAsFile = bool.Parse(ConfigurationManager
+                        .AppSettings["Email.WriteAsFile"] ?? "false")
+            };
+
+            kernel.Bind<IOrderProcessor>().To<EmailOrderProcessor>()
+                .WithConstructorArgument("settings", emailSettings);
         }
     }
 }
