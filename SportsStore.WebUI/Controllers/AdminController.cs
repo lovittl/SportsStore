c858368a4abe43bc691bcfcd5d16b30f6e0b90ae
Roy Lovitt <lovittl@my.erau.edu>
2018-05-09 06:21:07 -0400

    Add edit action method in adminController.cs p283

--- SportsStore.WebUI/Controllers/AdminController.cs
+++ SportsStore.WebUI/Controllers/AdminController.cs
@@ -1,5 +1,7 @@
-﻿using System.Web.Mvc;
+﻿using System.Linq;
+using System.Web.Mvc;
 using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
 
 namespace SportsStore.WebUI.Controllers
 {
@@ -8,13 +10,19 @@ namespace SportsStore.WebUI.Controllers
     {
         private IProductRepository repository;
 
-        public AdminController(IProductRepository repo) {
+        public AdminController(IProductRepository repo)
+        {
             repository = repo;
         }
 
         public ViewResult Index()
         {
             return View(repository.Products);
+        }
+
+        public ViewResult Edit(int productId)
+        { Product product = repository.Products.FirstOrDefault(p => p.ProductID == productId);
+            return View(product);
         }
     }
 }
